ff0a7040a4aa7bfa0f82f491ddcc659f10f65cff
john solis <1johnmichael.solis@gmail.com>
2017-09-30 20:32:19 -0700

    server configured

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ var passport = require('passport');
 
 require('dotenv');
 require('config/database');
-var app = require('express');
+var app = express();
 
-app.use(bodyParser.json());
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname,'build')));
+app.use(bodyParser.json());
+app.use(require('./config/passport'));
 
+//catch all route for routes
+app.get('/*', (req,res)=> {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+var port = process.env.PORT || 3001
 
+app.listen(port, ()=> {
+    console.log(`Express app running on port ${port}`)
+})
