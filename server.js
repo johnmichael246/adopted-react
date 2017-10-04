8463a03eb10a565ac71c74b8129cd7e51a18f9eb
john solis <1johnmichael.solis@gmail.com>
2017-10-03 23:02:50 -0700

    commit

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express=require('express');
+var express = require('express');
 var path = require('path');
 var methodOverride=require('method-override');
 var morgan = require('morgan');
@@ -12,12 +12,27 @@ var app = express();
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname,'build')));
 app.use(bodyParser.json());
-// app.use(require('./config/passport'));
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(cookieParser());
+// app.use(session({
+//     seret:'SaveALife!',
+//     resave:false,
+//     saveUnitialized:true
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //catch all route for routes
 app.get('/*', (req,res)=> {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 var port = process.env.PORT || 3001
 
 app.listen(port, ()=> {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {makeMainRoutes} from './routes'
-let routes = makeMainRoutes();
+import {makeMainRoutes} from './routes';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+// let routes = makeMainRoutes();
 
-ReactDOM.render(routes, document.getElementById('root'));
+// ReactDOM.render(routes, document.getElementById('root'));
+
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
